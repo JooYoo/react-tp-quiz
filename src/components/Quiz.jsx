7d413f923a787393cbc092c44f2dd31db756d6dc
JooYoo <jooyoo@outlook.com>
2021-06-29 09:13:55 +0200

    dev: add static quiz problems

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,42 @@
 import React from "react";
 
 const Quiz = () => {
+  // data source
+  const problems = [
+    {
+      id: 1,
+      question: "What is 1 + 1",
+      answers: [
+        { txt: "1", isCorrect: false },
+        { txt: "2", isCorrect: true },
+        { txt: "3", isCorrect: false },
+        { txt: "4", isCorrect: false }
+      ]
+    },
+    {
+      id: 2,
+      question: "What is 2 + 2",
+      answers: [
+        { txt: "2", isCorrect: false },
+        { txt: "3", isCorrect: false },
+        { txt: "4", isCorrect: true },
+        { txt: "5", isCorrect: false }
+      ]
+    },
+    {
+      id: 3,
+      question: "What is 3 + 3",
+      answers: [
+        { txt: "3", isCorrect: false },
+        { txt: "4", isCorrect: false },
+        { txt: "5", isCorrect: false },
+        { txt: "6", isCorrect: true }
+      ]
+    }
+  ];
+
+  // TODO: current problem index
+
   return (
     <>
       <div>quiz works</div>
